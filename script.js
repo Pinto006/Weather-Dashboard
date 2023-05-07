ce2e04f730d7f396fdeedb96b9c4da83ea872343
Wave FC <malloriepinto89@gmail.com>
2023-05-07 15:40:39 -0700

    working on API

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const tempEl = document.querySelector('.temp');
 const windEl = document.querySelector('.wind');
 const humidEl = document.querySelector('.humidity');
 const cityNameEl = document.querySelector('#cityName');
+const currentDayEl = document.querySelector('#currentDay');
 const weatherEl = document.querySelector('.weather');
 const searchBtn = document.getElementById('searchBtn');
-const key = '6d6d6cc45adb90e005966b8864829171';
+const requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=32.7157&lon=117.1611&appid=6d6d6cc45adb90e005966b8864829171";
 const Kelvin = 273;
 
 const weather = {
@@ -29,35 +30,42 @@ function setPosition(position) {
     getWeather(longitude, latitude);
 };
 
-function dispalyWeather () {
+function displayWeather () {
     cityNameEl.innerHTML = `${weather.city}`;
     tempEl.innerHTML = `${weather.temperature.value}`;
     windEl.innerHTML = `${weather.wind.value}`;
     humidEl.innerHTML = `${weather.humidity.value}`;
 };
-
+//convert temp to F
 function kelvIntoFahrenheit (temperature) {
   tempEl=((temperature-273.15) * 9/5 + 32)
 };
 
-function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=6d6d6cc45adb90e005966b8864829171`;
-    console.log(api);
-
-    fetch(api)
-        .then( function(response) {
-        let data = response.json();
-        return data;
+function getWeather(temp,wind,humidity) {
+    console.log(fetch(requestUrl))
+    fetch(requestUrl) //promise
+        .then(function(response) { // THEN run code 
+            console.log(response)
+        return response.json(); //returns another promise
     })
-    .then(function(data){
-        weather.temperature.value=Math.floor(data.main.temp - Kelvin);
-        weather.wind.value=data.wind.speed;
-        weather.humidity.value=data.main.humidity;
-        weather.city= data.name;
-    })
-    .then(function(){
-        dispalyWeather();
-    });
-};
+    .then(function(data){ // promise chain
+        console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var Temp = document.createElement('li');
+            // windEl = document.append('p');
+        // humidEl = document.append('p');
+     
+        Temp.textContent = data[i].html_url;
+        currentDayEl.appendChild(Temp);
+    }  
+    //     weather.temperature.value=Math.floor(data.main.temp - Kelvin);
+    //     weather.wind.value=data.wind.speed;
+    //     weather.humidity.value=data.main.humidity;
+    //     weather.city= data.name;
+    // })
+    // .then(function(){
+    //     displayWeather();
+    // });
+})};
 
 searchBtn.addEventListener('click', getWeather);
