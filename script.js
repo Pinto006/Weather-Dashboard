05fd0cd0b40d6ed70ef1fcf4db6c55e27dd3f55d
Wave FC <malloriepinto89@gmail.com>
2023-05-06 22:02:34 -0700

    update to JS, API not working

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const windEl = document.querySelector('.wind');
 const humidEl = document.querySelector('.humidity');
 const cityNameEl = document.querySelector('#cityName');
 const weatherEl = document.querySelector('.weather');
+const searchBtn = document.getElementById('searchBtn');
 const key = '6d6d6cc45adb90e005966b8864829171';
 const Kelvin = 273;
 
@@ -28,30 +29,34 @@ function setPosition(position) {
     getWeather(longitude, latitude);
 };
 
-// dispalyWeather (){
-//     cityNameEl.innerHTML = `${weather.city}`;
-//     tempEl.innerHTML = `${weather.temperature.value}`;
-//     windEl.innerHTML = `${weather.wind.value}`;
-//     humidEl.innerHTML = `${weather.humidity.value}`;
-// }
+function dispalyWeather () {
+    cityNameEl.innerHTML = `${weather.city}`;
+    tempEl.innerHTML = `${weather.temperature.value}`;
+    windEl.innerHTML = `${weather.wind.value}`;
+    humidEl.innerHTML = `${weather.humidity.value}`;
+};
 
 function kelvIntoFahrenheit (temperature) {
   tempEl=((temperature-273.15) * 9/5 + 32)
 };
 
 function getWeather(latitude, longitude) {
-    let api = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=6d6d6cc45adb90e005966b8864829171`;
+    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=6d6d6cc45adb90e005966b8864829171`;
     console.log(api);
-}
 
-//     fetch(api).then( function(response) {
-//         weather.temperature.value=Math.floor(data.main.temp - Kelvin);
-//         weather.humidity.value=data.main.humidity;
-//         weather.city= data.name;
-//         let data = response.json();
-//         return data;
-//     })
-//     .then(function(){
-//         dispalyWeather();
-//     });
-// }
+    fetch(api)
+        .then( function(response) {
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value=Math.floor(data.main.temp - Kelvin);
+        weather.humidity.value=data.main.humidity;
+        weather.city= data.name;
+    })
+    .then(function(){
+        dispalyWeather();
+    });
+};
+
+searchBtn.addEventListener('click', getWeather);
